2957151275b22ea189145cea828bdd6397af8ef0
Ben Firth <bfirth93@gmail.com>
2020-09-02 10:58:00 -0700

    Add form onchange methods

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+  const [location, addLocation] = useState('');
+  const [activity, addActivity] = useState('');
+  const [description, addDescription] = useState('');
+
+  const clickHandler = () => {
+    
+  };
+
   return (
     <section>
       <h3>Add a new adventure stop!</h3>
       <form>
         <label>Location</label>
-        <input />
+        <input 
+          value={location} 
+          onChange={(e) => addLocation(e.target.value)}
+        />
         <label>Activity</label>
-        <input />
+        <input 
+          value={activity} 
+          onChange={(e) => addActivity(e.target.value)}
+        />
         <label>Description</label>
-        <input />
-        <button type='button'>Submit Activity</button>
+        <input 
+          value={description} 
+          onChange={(e) => addDescription(e.target.value)}
+        />
+        <button 
+          onClick={clickHandler}
+          type='button'>Submit Activity
+        </button>
       </form>
     </section>
   );
