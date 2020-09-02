2149b4a94635fd7b4592380805fae3cf40533786
Ben Firth <bfirth93@gmail.com>
2020-09-02 10:58:06 -0700

    Add root reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import { activityReducer } from './activityReducer';
+
+
+export const rootReducer = combineReducers({
+  activities: activityReducer
+});
+
+export default rootReducer;
