a039ff3230ad0d35fabc636dfcfd19ba412b3c82
Ben Firth <bfirth93@gmail.com>
2020-09-02 09:36:30 -0700

    Create app component

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Header/Header';
+import IdeaContainer from '../IdeaContainer/IdeaContainer';
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+      <Header />
+      <IdeaContainer />
+    </main>
+  );
+};
+
+export default App;
