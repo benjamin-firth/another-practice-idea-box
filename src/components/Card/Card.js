5b3f37d23f2cdef2900c6bbcb13847ecd0da2922
Ben Firth <bfirth93@gmail.com>
2020-09-02 11:23:09 -0700

    Add activity dispatch

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ location, activityName, description }) => {
   return (
     <article>
-      <h3>Activity</h3>
-      <p>Location</p>
+      <h3>{activityName}</h3>
+      <p>{location}</p>
       <p>Description:</p>
+      <p>{description}</p>
     </article>
   );
 };
