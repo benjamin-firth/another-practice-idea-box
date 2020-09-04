64017fdbcbc87f3b6c0226b1eb10abc2f9e6ea63
Ben Firth <bfirth93@gmail.com>
2020-09-04 12:41:49 -0700

    Add example saga

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import "regenerator-runtime/runtime";
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { rootReducer } from './reducers';
 
-const store = createStore(rootReducer, composeWithDevTools())
+function* exampleSaga() {
+  console.log("Example saga reached");
+};
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(exampleSaga);
 
 ReactDOM.render(
   <Provider store={store}>
